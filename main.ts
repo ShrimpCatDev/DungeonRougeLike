1dca64584aece6f73aa1fa38aca1507303659399
ShrimpCatDev <shrimpcatboi223@outlook.com>
2024-10-18 18:02:08 -0500

    Attempting to implement autotiling

--- main.ts
+++ main.ts
@@ -52,6 +52,18 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         sprites.setDataBoolean(mySprite, "move", false)
     }
 })
+function autoTile () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile12`)) {
+        for (let index = 0; index <= directions.length - 1; index++) {
+            tempX = directions[index][0]
+            tempY = directions[index][1]
+            if (tiles.tileAtLocationEquals(tiles.getTileLocation(value.column + tempX, value.row + tempY), assets.tile`transparency8`)) {
+                tiles.setTileAt(tiles.getTileLocation(value.column + tempX, value.row + tempY), Tiles[index])
+                tiles.setWallAt(tiles.getTileLocation(value.column + tempX, value.row + tempY), true)
+            }
+        }
+    }
+}
 function move () {
     for (let value of sprites.allOfKind(SpriteKind.slime)) {
         timer.background(function () {
@@ -64,16 +76,39 @@ function move () {
         })
     }
 }
-let directions: number[][] = []
+let tempY = 0
+let tempX = 0
 let mySprite: Sprite = null
 let rng: FastRandomBlocks = null
+let directions: number[][] = []
+let Tiles: Image[] = []
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 128
     export const ARCADE_SCREEN_HEIGHT = 128
 }
-rng = Random.createRNG(game.askForNumber("seed", 3))
+Tiles = [
+assets.tile`myTile`,
+assets.tile`myTile7`,
+assets.tile`myTile10`,
+assets.tile`myTile1`,
+assets.tile`myTile11`,
+assets.tile`myTile0`,
+assets.tile`myTile8`,
+assets.tile`myTile2`
+]
+directions = [
+[0, -1],
+[0, 1],
+[-1, 0],
+[1, 0]
+]
+for (let value of sprites.allOfKind(SpriteKind.slime)) {
+    sprites.setDataNumber(value, "movedir", rng.randomRange(0, 3))
+}
+rng = Random.createRNG(randint(0, 999))
 let maps = [tileUtil.createSmallMap(tilemap`level2`)]
 tiles.setCurrentTilemap(maps[0])
+autoTile()
 for (let value of tiles.getTilesByType(assets.tile`myTile12`)) {
     if (rng.percentChance(4)) {
         tiles.setTileAt(value, assets.tile`myTile3`)
@@ -104,12 +139,3 @@ tileUtil.createSpritesOnTiles(assets.tile`myTile3`, img`
     . c c c c c c . 
     `, SpriteKind.slime)
 tileUtil.replaceAllTiles(assets.tile`myTile3`, assets.tile`myTile12`)
-directions = [
-[0, -1],
-[0, 1],
-[-1, 0],
-[1, 0]
-]
-for (let value of sprites.allOfKind(SpriteKind.slime)) {
-    sprites.setDataNumber(value, "movedir", rng.randomRange(0, 3))
-}
